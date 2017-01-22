4a24bd1c922357f748fd748893bee21fe97e2302
David T. Edwards <davidtedwards2017@gmail.com>
2017-01-22 12:11:24 -0800

    Completed Gameloop and end splashscreen

---
+++ Assets/Scripts/UI/EndTimeDisplay.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System;
+
+[RequireComponent(typeof(Text))]
+public class EndTimeDisplay : MonoBehaviour {
+
+    public Text minText;
+    public Text secText;
+    	
+	// Update is called once per frame
+	void Update () {
+        UpdateTime(TimeSpan.FromSeconds(GameController.instance.GameTime));
+
+	}
+
+    void UpdateTime(TimeSpan time)
+    {
+        var mins = (int) time.TotalMinutes;
+        var seconds = (int)time.TotalSeconds;
+
+        if (mins < 1)
+        {
+            minText.text = "";
+            secText.text = string.Format("{0}s", seconds);
+        }
+        else
+        {
+            minText.text = string.Format("{0}m", mins );
+            secText.text = string.Format("{0}s", seconds - (60 * mins));
+        }
+    }
+}
