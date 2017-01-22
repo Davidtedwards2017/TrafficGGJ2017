4b6c698a0469f7451013151fa3e37a5ca4c41569
Justin Pando <justin.pando@gmail.com>
2017-01-22 12:44:54 -0800

    Added Squid guy

---
+++ Assets/Scripts/Animation/PlayerAnimatorController.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(tk2dAnimatedSprite))]
+public class PlayerAnimatorController : MonoBehaviour {
+
+    public tk2dSpriteAnimator squidAnimator;
+
+    public tk2dSpriteAnimator northFacingTentacle;
+    public tk2dSpriteAnimator southFacingTentacle;
+    public tk2dSpriteAnimator westFacingTentacle;
+    public tk2dSpriteAnimator eastFacingTentacle;
+
+    public delegate void AnimationEventHandler(tk2dSpriteAnimator animator, tk2dSpriteAnimationClip clip, int frameNumber);
+    public event AnimationEventHandler onAnimationEventTriggered;
+
+    void Awake()
+    {
+        //squidAnimator = GetComponent<tk2dAnimatedSprite>();
+    }
+
+    void Start()
+    {
+        MessageController.StartListening("DirectionInputChanged", OnDirectionChanged);
+    }
+
+    void OnDirectionChanged(object[] args)
+    {
+        DataTypes.Direction direction = (DataTypes.Direction)args[0];
+        bool isOn = (bool)args[1];
+
+        Wave(direction, isOn);
+    }
+
+    public void Wave(DataTypes.Direction direction, bool isOn)
+    {
+        string animName = "";
+        tk2dSpriteAnimator tentacle = null;
+
+        switch (direction)
+        {
+            case DataTypes.Direction.North:
+                animName = isOn ? "ArmN_Wave" : "ArmN_Stop";
+                tentacle = northFacingTentacle;
+                break;
+            case DataTypes.Direction.South:
+                animName = isOn ? "ArmS_Wave" : "ArmS_Stop";
+                tentacle = southFacingTentacle;
+                break;
+            case DataTypes.Direction.West:
+                animName = isOn ? "ArmW_Wave" : "ArmW_Stop";
+                tentacle = westFacingTentacle;
+                break;
+            case DataTypes.Direction.East:
+                animName = isOn ? "ArmE_Wave" : "ArmE_Stop";
+                tentacle = eastFacingTentacle;
+                break;
+        }
+
+        PlayAnimation(tentacle, animName);
+    }
+
+    void PlayAnimation(tk2dSpriteAnimator anim, string name, bool mayTransitionToSameAnimation = false)
+    {
+        
+        if (anim.IsPlaying(name) && !mayTransitionToSameAnimation) return;
+
+        anim.Play(name);
+    }
+
+}
