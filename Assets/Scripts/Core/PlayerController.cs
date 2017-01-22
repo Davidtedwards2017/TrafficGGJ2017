c9a227365ab47d6f25c39936a552a9219098e331
David T. Edwards <davidtedwards2017@gmail.com>
2017-01-22 10:59:12 -0800

    Added splash screens and first stages of destruction

--- Assets/Scripts/Core/PlayerController.cs
+++ Assets/Scripts/Core/PlayerController.cs
@@ -1,7 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof(tk2dAnimatedSprite))]
 public class PlayerController : MonoBehaviour {
+
+    tk2dAnimatedSprite SquidAnimator;
+
+    public TenticleAnimator NorthFacingTenticle;
+    public TenticleAnimator EastFacingTenticle;
+    public TenticleAnimator SouthFacingTenticle;
+    public TenticleAnimator WestFacingTenticle;
+
+    void Awake()
+    {
+        SquidAnimator = GetComponent<tk2dAnimatedSprite>();
+    }
 
 	// Use this for initialization
 	void Start () {
---
+++ Assets/Scripts/DestructionFade.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class DestructionFade : MonoBehaviour {
+
+    Image fadeImage;
+	// Use this for initialization
+	void Start () {
+        fadeImage = GetComponent<Image>();
+        GameController.instance.Destruction.OnValueChanged += OnDestuctionUpdated;
+        OnDestuctionUpdated();
+
+    }
+	
+    void OnDestuctionUpdated()
+    {
+        var destruction = GameController.instance.Destruction.value;
+
+        fadeImage.DOFade(destruction, 0.1f);
+    }
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
