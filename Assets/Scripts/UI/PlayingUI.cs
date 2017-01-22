d7788dc83365a32ca159f66bc6a2bc34d5d0eda7
David T. Edwards <davidtedwards2017@gmail.com>
2017-01-21 18:55:34 -0800

    Added canvas hooks

---
+++ Assets/Scripts/UI/PlayingUI.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayingUI : UiController {
+
+    protected override void Awake()
+    {
+        base.Awake();
+        GameController.state.values[GameController.State.Playing].OnEnter += Show;
+        GameController.state.values[GameController.State.Playing].OnExit += Hide;
+    }
+}
---
+++ Assets/Scripts/UI/UiController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public abstract class UiController : MonoBehaviour {
+
+    public CanvasGroup Group;
+
+    protected virtual void Awake()
+    {
+        Group = GetComponent<CanvasGroup>();
+    }
+
+    protected void Show()
+    {
+        Group.alpha = 1;
+    }
+
+    protected void Hide()
+    {
+        Group.alpha = 0;
+    }
+}
