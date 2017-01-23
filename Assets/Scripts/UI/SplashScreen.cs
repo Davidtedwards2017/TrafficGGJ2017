db273cf858222505bd4747a12d1b61fccad97678
David T. Edwards <davidtedwards2017@gmail.com>
2017-01-22 16:49:18 -0800

    Added title splash screen

--- Assets/Scripts/UI/SplashScreen.cs
+++ Assets/Scripts/UI/SplashScreen.cs
@@ -7,19 +7,24 @@ public class SplashScreen : Singleton<SplashScreen> {
 
     public float FadeDuration = 1.0f;
     public float HoldDuration = 2.0f;
-    CanvasGroup group;
-    
+    public CanvasGroup group1;
+    public CanvasGroup group2;
+
     void Awake()
     {
-        group = GetComponent<CanvasGroup>();
-        group.alpha = 1;
+        group1.alpha = 1;
+        group2.alpha = 0;
     }
 
 
     public IEnumerator DisplaySequence()
     {
         yield return new WaitForSeconds(HoldDuration);
-        yield return group.DOFade(0, FadeDuration);
+        group1.DOFade(0, FadeDuration);
+        group2.DOFade(1, FadeDuration);
+        yield return new WaitForSeconds(FadeDuration);
+        yield return new WaitForSeconds(HoldDuration);
+        yield return group2.DOFade(0, FadeDuration);
     }
 
 }
